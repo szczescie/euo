a7018fd4bc2ad16cf2221e5419201fa41b755604
szczescie <105247530+szczescie@users.noreply.github.com>
2025-08-22 20:25:18 +0200

    update tests

--- src/tests.c
+++ src/tests.c
@@ -1,66 +1,72 @@
-#include "assert.h"
+#include <assert.h>
+#include <stdint.h>
 
-#define euo_error_type unsigned int
-#define euo_types int, long, char const*, short*, bool
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define euo_error_type u32
+#define euo_types u32, u64, u8, u8 const*, u16*, bool
 #include "euo.h"
 
-static Err(long) err_union() {
-    static_assert(sizeof(Err(long)) == 2 * sizeof(long));
+static Err(u64) err_union() {
+    static_assert(sizeof(Err(u64)) == 2 * sizeof(u64));
     {
-        Err(int) const int_or_err = ok(-0xaaaaaaa);
-        bool const failure = failed(int_or_err);
+        Err(u32) const u32_or_err = ok((u32)0xaaaaaaa);
+        bool const failure = failed(u32_or_err);
         assert(!failure);
-        long const unwrapped = val(int_or_err);
-        assert(unwrapped == -0xaaaaaaa);
-    }
-    {
-        Err(long) const long_or_err = err(long)(404);
-        assert(failed(long_or_err));
-        unsigned int const error_code = errcode(long_or_err);
-        assert(error_code == 404);
-        return long_or_err;
-    }
-}
-
-static Opt(short*) optional() {
-    static_assert(sizeof(Err(short*)) == 2 * sizeof(short*));
-    {
-        Opt(char const*) const str_or_null = some((char const*){ "aaaaaaa" });
-        bool const absence = absent(str_or_null);
-        assert(!absence);
-        char const* const unwrapped = val(str_or_null);
-        assert(unwrapped == (char const*){ "aaaaaaa" });
-    }
-    {
-        Opt(short*) const short_ptr_or_null = none(short*);
-        bool const absence = absent(short_ptr_or_null);
-        assert(absence);
-        return short_ptr_or_null;
-    }
-}
-
-static Err(Opt(char const*)) err_optional() {
-    static_assert(sizeof(Err(Opt(char const*))) == sizeof(Err(char const*)));
-    {
-        Err(Opt(int)) const int_or_null_or_err = ok(some(0xaaaaaaa));
-        bool const failure = failed(int_or_null_or_err);
-        assert(!failure);
-        Opt(int) const int_or_null = val(int_or_null_or_err);
-        bool const absence = absent(int_or_null);
-        assert(!absence);
-        int const unwrapped = val(int_or_null);
+        u64 const unwrapped = val(u32_or_err);
         assert(unwrapped == 0xaaaaaaa);
     }
     {
-        Err(Opt(long)) const long_or_null_or_err = ok(none(long));
-        bool const failure = failed(long_or_null_or_err);
+        Err(u64) const u64_or_err = err(u64)(404);
+        assert(failed(u64_or_err));
+        u32 const error_code = errcode(u64_or_err);
+        assert(error_code == 404);
+        return u64_or_err;
+    }
+}
+
+static Opt(u16*) optional() {
+    static_assert(sizeof(Err(u16*)) == 2 * sizeof(u16*));
+    {
+        Opt(u8) const u8_or_null = some((u8)0xb);
+        bool const absence = absent(u8_or_null);
+        assert(!absence);
+        u8 const unwrapped = val(u8_or_null);
+        assert(unwrapped == (u8)0xb);
+    }
+    {
+        Opt(u16*) const u16_ptr_or_null = none(u16*);
+        bool const absence = absent(u16_ptr_or_null);
+        assert(absence);
+        return u16_ptr_or_null;
+    }
+}
+
+static Err(Opt(u8 const*)) err_optional() {
+    static_assert(sizeof(Err(Opt(u8 const*))) == sizeof(Err(u8 const*)));
+    {
+        Err(Opt(u32)) const u32_or_null_or_err = ok(some((u32)0xaaaaaaa));
+        bool const failure = failed(u32_or_null_or_err);
         assert(!failure);
-        Opt(long) const long_or_null = val(long_or_null_or_err);
-        bool const absence = absent(long_or_null);
+        Opt(u32) const u32_or_null = val(u32_or_null_or_err);
+        bool const absence = absent(u32_or_null);
+        assert(!absence);
+        u32 const unwrapped = val(u32_or_null);
+        assert(unwrapped == 0xaaaaaaa);
+    }
+    {
+        Err(Opt(u64)) const u64_or_null_or_err = ok(none(u64));
+        bool const failure = failed(u64_or_null_or_err);
+        assert(!failure);
+        Opt(u64) const u64_or_null = val(u64_or_null_or_err);
+        bool const absence = absent(u64_or_null);
         assert(absence);
     }
     {
-        Err(Opt(char const*)) str_or_null_or_err = err(Opt(char const*))(503);
+        Err(Opt(u8 const*)) str_or_null_or_err = err(Opt(u8 const*))(503);
         bool const failure = failed(str_or_null_or_err);
         assert(failure);
         return str_or_null_or_err;
@@ -68,7 +74,7 @@ static Err(Opt(char const*)) err_optional() {
 }
 
 static Err(bool) err_union_try() {
-    int const number = __extension__ try(bool)(ok(0xaaaaaaa));
+    u32 const number = __extension__ try(bool)(ok((u32)0xaaaaaaa));
     assert(number == 0xaaaaaaa);
     (void)__extension__ try(bool)(err_union());
     assert(false);
